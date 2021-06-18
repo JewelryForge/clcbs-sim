81f149557c46be8ab334c97269e1621dcd41a977
Chengrui Zhu <3180105772@zju.edu.cn>
2021-06-18 20:29:30 +0800

    admit a series of robots to load in gazebo!!!

--- clcbs_gazebo/scripts/robot_tf.py
+++ clcbs_gazebo/scripts/robot_tf.py
@@ -15,13 +15,12 @@ class GazeboLinkPose:
     def __init__(self, robot_name, link_name):
         self.robot_name = robot_name
         self.link_name = link_name
-        # self.link_name_rectified = link_name.replace("::", "_")
 
         if not self.link_name:
             raise ValueError("'link_name' is an empty string")
 
         self.states_sub = rospy.Subscriber("/gazebo/link_states", LinkStates, self.callback)
-        self.pose_pub = rospy.Publisher(f'/agent_states/{self.robot_name}_{self.link_name}', Pose, queue_size=10)
+        self.pose_pub = rospy.Publisher(f'/agent_states/{self.robot_name}/{self.link_name}', Pose, queue_size=10)
         self.tf_pub = tf.TransformBroadcaster()
 
     def callback(self, data):
@@ -35,7 +34,7 @@ class GazeboLinkPose:
         p, o = self.link_pose.position, self.link_pose.orientation
         translation = [p.x, p.y, p.z]
         rotation = [o.x, o.y, o.z, o.w]
-        if abs(math.hypot(*rotation) - 1) > 0.1:
+        if abs(math.hypot(*rotation) - 1) > 0.01:
             rotation = [0, 0, 0, 1]
         self.tf_pub.sendTransform(translation, rotation, rospy.Time.now(), self.link_name, "map")
         print('tf published')
