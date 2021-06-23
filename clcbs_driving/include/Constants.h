1ce955467677db4e9b9c5a29f1d000cf5220aca1
Chengrui Zhu <3180105772@zju.edu.cn>
2021-06-23 16:36:52 +0800

    Add Constants.h

---
+++ clcbs_driving/include/Constants.h
@@ -0,0 +1,16 @@
+#include <string>
+
+
+namespace Constants {
+
+static double MINIMUM_ROTATION_RADIUS = 3.0;
+static double MAXIMUM_LINEAR_VELOCITY = 4.0;
+static double WHEEL_RADIUS = 0.5;
+static double CAR_WIDTH = 1.0;
+static double CAR_LENGTH = 2.0;
+static double MAP_SIZE_X = 50.0;
+static double MAP_SIZE_Y = 50.0;
+
+void loadFromRosParam();
+void loadFromYamlFile(std::string);
+}
